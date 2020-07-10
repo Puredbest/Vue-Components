2f6cac86cb6e315f7e9fc3cf6ca16dd7fe5da105
Robert Jones <rdj4318@ic.ac.uk>
2020-07-10 11:09:27 +0100

    Mouse enlarges objects and fast moving objects go white

--- src/components/Canvas/Canvas.vue
+++ src/components/Canvas/Canvas.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-         <canvas id="front-animation"></canvas>
+    <div id="parent" style="height:70vh;">
+         <canvas id="front-animation" style="width:100%; height:100%"></canvas>
     </div>
 </template>
 
@@ -9,8 +9,11 @@ export default {
     name: 'iv-front-page',
     mounted(){
         let canvas = document.querySelector('canvas');
-        canvas.width = window.innerWidth*0.65;
-        canvas.height = window.innerHeight*0.8;
+        // canvas.width = window.innerWidth*0.65;
+        // canvas.height = window.innerHeight*0.8;
+        let parent = document.getElementById('parent');
+        canvas.width = parent.offsetWidth;
+        canvas.height = parent.offsetHeight;
 
         let c = canvas.getContext('2d');
 
@@ -51,6 +54,24 @@ export default {
         //         yPositions.push(j*canvas.height/ballNumRoot + radius);
         //     }
         // }
+        var mouse = {
+            x: undefined,
+            y: undefined
+        }
+        
+        let rect = parent.getBoundingClientRect();
+        let timer;
+
+        parent.addEventListener('mousemove', function(event){
+            mouse.x = event.x - rect.left;
+            mouse.y = event.y - rect.top;
+
+            clearInterval(timer);
+            timer = setInterval(function(){mouse.x = undefined; mouse.y = undefined}, 10000);
+            
+
+        })
+
 
         function rotateShape(angle, centre, points){
             // console.log(angle, centre, points);
@@ -62,9 +83,10 @@ export default {
             return points;
         }
         
-        function Ball(x, y) {
+        function Ball(x, y, defaultRadius) {
             this.x = x;
             this.y = y;
+            this.radius = defaultRadius;
 
             //make all orbit in circular orbits in same direction initially 
             this.dx = 0;
@@ -72,22 +94,24 @@ export default {
 
             for(let i = 0; i < massCentres.length; i++){  
                 this.dist = Math.pow(Math.pow(this.x-massCentres[i][0] , 2) + Math.pow(this.y-massCentres[i][1], 2), 0.5);
-                this.dy += -initialVel*Math.abs(Math.pow(this.dist , -2))*(massCentres[i][0]-this.x);
+                this.dy += -initialVel*massCentres[i][2]*Math.abs(Math.pow(this.dist , -2))*(massCentres[i][0]-this.x);
             }
             for(let i = 0; i < massCentres.length; i++){
                 this.dist = Math.pow(Math.pow(this.x-massCentres[i][0] , 2) + Math.pow(this.y-massCentres[i][1], 2), 0.5);
-                this.dx += initialVel*Math.abs(Math.pow(this.dist , -2))*(massCentres[i][1]-this.y);
+                this.dx += initialVel*massCentres[i][2]*Math.abs(Math.pow(this.dist , -2))*(massCentres[i][1]-this.y);
             }
 
             this.draw = function() {
-                let points = rotateShape(this.angle, [this.x, this.y], [[0, radius], [-0.5*radius, -radius], [0.5*radius, -radius]]);
+                let points = rotateShape(this.angle, [this.x, this.y], [[0, this.radius], [-0.5*this.radius, -this.radius], [0.5*this.radius, -this.radius]]);
 
                 c.beginPath();
                 c.moveTo(points[0][0], points[0][1]);
                 c.lineTo(points[1][0], points[1][1]);
                 c.lineTo(points[2][0], points[2][1]);
-                //c.arc(this.x, this.y, radius, 0, Math.PI * 2);
-                c.fillStyle = 'blue';
+                //c.fillStyle = 'blue';
+                let b = 255*Math.pow(Math.pow(this.dx,2) + Math.pow(this.dy, 2), 0.25);
+                //console.log(b);
+                c.fillStyle = 'rgb('+ b + ',' + b + ',255)';
                 c.fill(); 
             }
 
@@ -107,12 +131,24 @@ export default {
                     let dist = Math.pow(Math.pow(this.x-massCentres[i][0] , 2) + Math.pow(this.y-massCentres[i][1], 2), 0.5);
 
                     if(this.x - massCentres[i][0] !== 0){
-                        this.dx += 1*Math.abs(Math.pow(dist , -2))*(massCentres[i][0]-this.x);
+                        this.dx += massCentres[i][2]*Math.abs(Math.pow(dist , -2))*(massCentres[i][0]-this.x);
                     }
                     if(this.y - massCentres[i][1] !== 0){
-                        this.dy += 1*Math.abs(Math.pow(dist , -2))*(massCentres[i][1]-this.y);
+                        this.dy += massCentres[i][2]*Math.abs(Math.pow(dist , -2))*(massCentres[i][1]-this.y);
                     }
                 }
+
+                //interactivity with mouse
+                if(mouse.x - this.x < mouseRadius && mouse.x - this.x > -mouseRadius
+                && mouse.y - this.y < mouseRadius && mouse.y - this.y > -mouseRadius
+                ){
+                    if(this.radius < maxRadius){
+                        this.radius += 0.5;
+                    }
+                } else if (this.radius > defaultRadius) {
+                    this.radius -= 0.5;
+                }
+
                 
 
                 this.draw();
@@ -120,18 +156,23 @@ export default {
         }
         
         
-        let radius = 2;
+        let radius = canvas.height/150;
+        let maxRadius = 30;
+        let mouseRadius = 50;
         let ballArray = [];
         let ballNumRoot = 40;
-        let massCentres = [[canvas.width/3, canvas.height/3], [2*canvas.width/3, canvas.height/3], [canvas.width/2, 2*canvas.height/3]];
-        let initialVel = 100;
+
+        // [[x,y, mass]]
+        //let massCentres = [[canvas.width/3, canvas.height/3], [2*canvas.width/3, canvas.height/3], [canvas.width/2, 2*canvas.height/3]];
+        let massCentres = [[canvas.width/2, canvas.height/2 , 0.4]];
+        let initialVel = 50;
 
         for (let i = 0; i < ballNumRoot; i++){
             for (let j = 0; j < ballNumRoot; j++){
                 let x = i*canvas.width/ballNumRoot + radius;
                 let y = j*canvas.height/ballNumRoot + radius;
                 
-                ballArray.push(new Ball(x,y));
+                ballArray.push(new Ball(x,y, radius));
             }
         }
 
@@ -144,24 +185,23 @@ export default {
                 ballArray[i].update();
             }
 
-            for (let i = 0; i <massCentres.length; i++){
-                c.beginPath();
-                c.arc(massCentres[i][0], massCentres[i][1], 5, 0, Math.PI * 2); 
-                c.fillStyle = 'black';
-                c.fill(); 
-            }
+            // for (let i = 0; i <massCentres.length; i++){
+            //     c.beginPath();
+            //     c.arc(massCentres[i][0], massCentres[i][1], 5, 0, Math.PI * 2); 
+            //     c.fillStyle = 'black';
+            //     c.fill();
+            // }
             
         }
 
         animate();
-    },
-
+    }
 
 }
 </script>
 
 <style>
 #front-animation{
-    border: 1px solid black;
+    /* border: 1px solid black; */
 }
 </style>
