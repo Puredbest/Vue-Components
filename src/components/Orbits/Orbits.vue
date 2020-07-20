05928862f735966ce2f242fe2d7ca42814c7ae35
Robert Jones <rdj4318@ic.ac.uk>
2020-07-20 09:18:36 +0100

    Dodgy auto scale

--- src/components/Orbits/Orbits.vue
+++ src/components/Orbits/Orbits.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="parent" style="height:70vh;" @mousemove="mouseOver" @mouseleave="mouseLeave">
+    <div id="parent" style="height:70vh;" @mousemove="mouseOver" @mouseleave="mouseLeave" @mousedown="activateSelect" @mouseup="cancelSelect">
          <canvas id="front-animation" style="width:100%; height:100%" ></canvas>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'iv-front-page',
+    name: 'iv-orbits',
     props: {
         mouse: {
             default: {
@@ -17,6 +17,9 @@ export default {
         animationSpeed: {
             default: 1
         },
+        velocitySelect: {
+            default: false
+        }
     },
     methods: {
         mouseOver(event){
@@ -26,6 +29,14 @@ export default {
         mouseLeave(){
             this.mouse.x = undefined;
             this.mouse.y = undefined;
+        },
+        activateSelect(){
+            this.velocitySelect = true;
+            console.log('activated');
+        },
+        cancelSelect(){
+            this.velocitySelect = false;
+            console.log('released');
         }
     },
     mounted(){
@@ -81,35 +92,66 @@ export default {
                 let stepSize = 1;
                 this.scale = 1;
                 this.closedPath = true;
+                let reverseMultiplier = 1;
                 //c.scale(0.5,0.5);
                 console.log('path drawing');
 
                 while(Math.pow((Math.pow(this.x-currentPos[0], 2) + Math.pow(this.y-currentPos[1],2)),0.5) > 1 || iterations < 100){
-
-                    if(currentPos[0] < (canvas.width - canvas.width*this.scale) || currentPos[0] > canvas.width*this.scale || currentPos[1] < (canvas.height - canvas.height*this.scale) || currentPos[1] > canvas.height*this.scale){
+                    
+                    if(currentPos[0] < (canvas.width - canvas.width*(1/this.scale))*(1/this.scale) || currentPos[0] > canvas.width*(1/this.scale)*(1/this.scale)|| currentPos[1] < (canvas.height - canvas.height*(1/this.scale))*(1/this.scale) || currentPos[1] > canvas.height*(1/this.scale)*(1/this.scale)){
+                        
+                        console.log('scale = ', this.scale);
+                        console.log(currentPos[0], (canvas.width - canvas.width*(1/this.scale)), canvas.width*(1/this.scale));
+                        console.log(currentPos[1], (canvas.height - canvas.height*(1/this.scale)) , canvas.height*(1/this.scale));
+                        
                         c.translate(canvas.width/4, canvas.height/4);
-                        this.scale = this.scale*2;
-                        c.scale(1/this.scale, 1/this.scale);
-                        console.log('rescale');
+                        this.scale = this.scale/2;
+                        c.scale(this.scale, this.scale);
+                        
+                        //console.log('newScale = ', canvas.width/currentPos[0]);
+                        // this.scale = Math.pow(Math.max(currentPos[0]/(canvas.width) , currentPos[1]/(canvas.height)), -1);
+                        
+                        //console.log('scale =', this.scale);
+                        //c.scale(Math.pow(this.scale, 1), Math.pow(this.scale,1));
+                        //console.log(Math.pow(this.scale, -1));
+                        //console.log('rescale');
                     }
 
 
-                    currentPos[0] += currentVel[0] * stepSize;
-                    currentPos[1] += currentVel[1] * stepSize;
+                    currentPos[0] += currentVel[0] * reverseMultiplier * stepSize;
+                    currentPos[1] += currentVel[1] * reverseMultiplier * stepSize;
                     
                     //console.log(currentPos, currentVel);
 
                     for(let i = 0; i < massCentres.length; i++){
                         let dist = Math.pow(Math.pow(currentPos[0]-massCentres[i][0] , 2) + Math.pow(currentPos[1]-massCentres[i][1], 2), 0.5);
 
-                        currentVel[0] = currentVel[0] + massCentres[i][2]*Math.abs(Math.pow(dist , -3))*(massCentres[i][0]-currentPos[0]);
-                        currentVel[1] = currentVel[1] + massCentres[i][2]*Math.abs(Math.pow(dist , -3))*(massCentres[i][1]-currentPos[1]);
+                        currentVel[0] = currentVel[0] + reverseMultiplier * massCentres[i][2]*Math.abs(Math.pow(dist , -3))*(massCentres[i][0]-currentPos[0]);
+                        currentVel[1] = currentVel[1] + reverseMultiplier * massCentres[i][2]*Math.abs(Math.pow(dist , -3))*(massCentres[i][1]-currentPos[1]);
                     }
 
-                    this.pathCoords.push(currentPos.slice());
-                    iterations += 1;
+                    if (this.closedPath){
+                        this.pathCoords.push(currentPos.slice());
+                    } else{
+                        this.pathCoords.unshift(currentPos.slice());
+                    }
 
-                    if(iterations > 10000){this.closedPath = false; break; }
+                    iterations += 1;
+                    
+                    //Draw reverse path for open path
+                    if(iterations > 10000){
+                        this.closedPath = false;
+                        currentPos = [this.x, this.y];
+                        currentVel = [this.dx, this.dy];
+                        iterations = 0;
+                        
+                        if(reverseMultiplier < 0){
+                            break;
+                        }
+
+                        reverseMultiplier = -1;
+                        console.log('reverse');
+                    }
                 }
 
             }
@@ -154,6 +196,10 @@ export default {
                     this.radius -= 0.5;
                 }
 
+                if(vm.velocitySelect && Math.pow((Math.pow((mouseX-this.x),2) + Math.pow((mouseY-this.y),2)),1/2) < mouseRadius){
+                    vm.animationSpeed = 0; 
+                }
+
                 this.draw();
             }
         }
@@ -165,7 +211,7 @@ export default {
         // [[x,y, mass]]
         //let massCentres = [[canvas.width/3, canvas.height/3], [2*canvas.width/3, canvas.height/3], [canvas.width/2, 2*canvas.height/3]];
         let massCentres = [[canvas.width/2, canvas.height/2 , 100]];
-        let initialVel = 1.9;
+        let initialVel = 1.3;
 
         ballArray.push(new Ball(canvas.width/3, canvas.height/3, 5));
         for(let i = 0; i < ballArray.length; i++){
@@ -183,7 +229,16 @@ export default {
             requestAnimationFrame(animate);
 
             for (let i = 0; i < ballArray.length; i++){    
-                c.clearRect((canvas.width-canvas.width*ballArray[i].scale),(canvas.height-canvas.height*ballArray[i].scale), canvas.width*ballArray[i].scale*2, canvas.height*ballArray[i].scale*2);
+                c.clearRect((canvas.width-canvas.width*(1/ballArray[i].scale))*(1/ballArray[i].scale),(canvas.height-canvas.height*(1/ballArray[i].scale))*(1/ballArray[i].scale), canvas.width*(1/ballArray[i].scale)*2*(1/ballArray[i].scale), canvas.height*(1/ballArray[i].scale)*2*(1/ballArray[i].scale));
+            //     bounding box
+            //     c.beginPath();
+            //     c.moveTo((canvas.width - canvas.width*(1/ballArray[i].scale))*(1/ballArray[i].scale), (canvas.height - canvas.height*(1/ballArray[i].scale))*(1/ballArray[i].scale));
+            //     c.lineTo(canvas.width*(1/ballArray[i].scale)*(1/ballArray[i].scale), (canvas.height - canvas.height*(1/ballArray[i].scale))*(1/ballArray[i].scale));
+            //     c.lineTo(canvas.width*(1/ballArray[i].scale)*(1/ballArray[i].scale), canvas.height*(1/ballArray[i].scale)*(1/ballArray[i].scale));
+            //     c.lineTo((canvas.width - canvas.width*(1/ballArray[i].scale))*(1/ballArray[i].scale), canvas.height*(1/ballArray[i].scale)*(1/ballArray[i].scale));
+            //     c.lineTo((canvas.width - canvas.width*(1/ballArray[i].scale))*(1/ballArray[i].scale), (canvas.height - canvas.height*(1/ballArray[i].scale))*(1/ballArray[i].scale));
+            //     c.lineWidth = 100;
+            //     c.stroke();
             }
 
             mouseX = vm.mouse.x - rect.left;
@@ -200,7 +255,11 @@ export default {
                 c.fill();
             }
 
-
+            
+            // c.beginPath();
+            // c.arc(2316, 669, 500, 0, Math.PI * 2); 
+            // c.fillStyle = 'Red';
+            // c.fill();
             
         }
 
